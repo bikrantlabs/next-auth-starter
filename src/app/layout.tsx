f18327a330625aa400abdad8290325485468f53c
bjung <bikrantjung1214@gmail.com>
2024-01-16 23:18:11 +0545

    feat:theme changer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,9 @@ import { Inter } from "next/font/google"
 
 import "./globals.css"
 
+import { ThemeChanger } from "@/components/features/theme-changer"
+import { ThemeProvider } from "@/components/theme-provider"
+
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
@@ -17,7 +20,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <ThemeChanger />
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   )
 }
