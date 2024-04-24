a46a97f38e24171af66f4b7736be9d8c3ac1b334
iambikrant <nerdbikrant007@gmail.com>
2024-04-24 13:37:44 +0545

    feat(auth): implement logout functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,35 @@
+import { auth, signOut } from "@/lib/auth"
 import { Button } from "@/components/ui/button"
 import { LoginButton } from "@/components/auth/login-button"
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth()
+  console.log(`🔥 page.tsx:7 ~ Session ~`, session)
   return (
     <main className="flex h-full flex-col items-center justify-center ">
       <div className="space-y-6 text-center">
         <h1 className="text-6xl font-semibold drop-shadow-md">Auth</h1>
         <p className="text-lg">Authentication service</p>
         <div>
-          <LoginButton>
-            <Button>Sign in</Button>
-          </LoginButton>
+          {session?.user ? (
+            /**
+             * We cannot directly do `<button onClick={signOut()}>`, since signOut() function modifies cookies
+             *
+             * Cookies can only be modified in a Server Action or Route Handler
+             */
+            <form
+              action={async () => {
+                "use server"
+                await signOut()
+              }}
+            >
+              <Button type="submit">Logout</Button>
+            </form>
+          ) : (
+            <LoginButton>
+              <Button>Sign in</Button>
+            </LoginButton>
+          )}
         </div>
       </div>
     </main>
